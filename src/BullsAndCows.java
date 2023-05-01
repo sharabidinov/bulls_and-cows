e043a50eec20a5c9b13e152bad6dac92d619f5fa
Alymbek Sharabidinov <alymbeksharabidinov1@gmail.com>
2023-05-01 12:04:15 +0600

    stage 2/7: Grader

--- src/BullsAndCows.java
+++ src/BullsAndCows.java
@@ -1,15 +1,35 @@
+import java.util.Scanner;
+
 public class BullsAndCows {
     public static void main(String[] args) {
-        System.out.println("""
-                The secret code is prepared: ****.
+        Scanner sc = new Scanner(System.in);
+        int bulls = 0;
+        int cows = 0;
+        String secretCode = "9305";
+        String str = sc.nextLine();
 
-                Turn 1. Answer:
-                1234
-                Grade: None.
+        for (int i = 0; i < str.length(); i++) {
+            char strDigit = str.charAt(i);
+            char secretCodeDigit = secretCode.charAt(i);
+            if (strDigit == secretCodeDigit) {
+                bulls++;
+            } else if (secretCode.indexOf(strDigit) != -1) {
+                cows++;
+            }
+        }
 
-                Turn 2. Answer:
-                9876
-                Grade: 4 bulls.
-                Congrats! The secret code is 9876.""");
+        if (bulls > 0 || cows > 0) {
+            String result = String.format("Grade: %d bull(s) and %d cow(s). The secret code is %s.", bulls, cows,
+                    secretCode);
+            System.out.println(result);
+        } else if (cows == 0) {
+            String result = String.format("Grade: %d bull(s). The secret code is %s.", bulls, secretCode);
+            System.out.println(result);
+        } else if (bulls == 0) {
+            String result = String.format("Grade: %d cows(s). The secret code is %s.", cows, secretCode);
+            System.out.println(result);
+        } else {
+            System.out.println("Grade: None. The secret code is 9305.");
+        }
     }
 }
